8481730014ad025a2f3a709d31f5c1ab6a422184
listennatural <2318046679@qq.com>
2018-11-19 14:27:03 +0800

    修改模型，新增购物车表，部分表添加新字段

--- mall/models.py
+++ mall/models.py
@@ -10,7 +10,9 @@ class Users(models.Model):
 
 
 class UserAddress(models.Model):
-    user = models.ForeignKey("Users", on_delete=models.SET_NULL, null=True, help_text="用户")
+    user = models.ForeignKey("Users", on_delete=models.CASCADE, help_text="用户")
+    name = models.CharField(max_length=255, help_text="联系人姓名")
+    phone = models.CharField(max_length=255, help_text="联系人电话")
     national = models.CharField(max_length=255, help_text="国家")
     province = models.CharField(max_length=255, help_text="省")
     city = models.CharField(max_length=255, help_text="市")
@@ -21,10 +23,17 @@ class UserAddress(models.Model):
     createtime = models.CharField(max_length=255, help_text="创建时间")
 
 
+class ShopCart(models.Model):
+    user = models.ForeignKey("Users", on_delete=models.CASCADE, help_text="用户")
+    good = models.ForeignKey("Goods", on_delete=models.CASCADE, help_text="商品")
+    now_price = models.FloatField(help_text="添加时的价格")
+    createtime = models.CharField(max_length=255, help_text="创建时间")
+
+
 class UserOrders(models.Model):
-    user = models.ForeignKey("Users", on_delete=models.SET_NULL, null=True, help_text="用户")
+    user = models.ForeignKey("Users", on_delete=models.CASCADE, help_text="用户")
     number = models.CharField(max_length=255, help_text="订单号", unique=True)
-    status = models.IntegerField(help_text="状态",default=0)
+    status = models.IntegerField(help_text="状态", default=0)
     total_price = models.FloatField(help_text="总价格")
     actual_payment = models.FloatField(help_text="实际支付")
     coupon = models.ForeignKey("Coupons", on_delete=models.CASCADE)
@@ -32,10 +41,10 @@ class UserOrders(models.Model):
 
 
 class OrderGoods(models.Model):
-    user = models.ForeignKey("Users", on_delete=models.SET_NULL, null=True, help_text="用户")
+    user = models.ForeignKey("Users", on_delete=models.CASCADE, help_text="用户")
     order = models.ForeignKey("UserOrders", on_delete=models.CASCADE, help_text="订单")
     good = models.ForeignKey("Goods", on_delete=models.CASCADE, help_text="商品")
-    original_price = models.FloatField(help_text="原价")
+    original_price = models.FloatField(help_text="原价", default=0.00)
     actual_price = models.FloatField(help_text="实际价格")
 
 
@@ -43,15 +52,17 @@ class Goods(models.Model):
     name = models.CharField(max_length=255, help_text="商品名")
     merchant = models.ForeignKey("Merchants", on_delete=models.SET_NULL, null=True, help_text="商家")
     img = models.CharField(max_length=255, help_text="图片")
-    original_price = models.FloatField(help_text="原价")
+    original_price = models.FloatField(help_text="原价", default=0.00)
     actual_price = models.FloatField(help_text="实际价格")
+    stock = models.IntegerField(help_text="库存", default=0)
+    status = models.IntegerField(help_text="状态", default=0)
     createtime = models.CharField(max_length=255, help_text="创建时间")
 
 
 class Merchants(models.Model):
     name = models.CharField(max_length=255, help_text="商家名称")
     city = models.CharField(max_length=255, help_text="所在城市")
-    level = models.ForeignKey("Levels", on_delete=models.SET_DEFAULT, default=1)
+    level = models.ForeignKey("Levels", on_delete=models.SET_DEFAULT, default=0)
     createtime = models.CharField(max_length=255, help_text="创建时间")
 
 
