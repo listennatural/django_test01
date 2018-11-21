134870ad4fcc87671b3bab55120b9e6c42a224df
listennatural <2318046679@qq.com>
2018-11-21 15:35:44 +0800

    修改后台管理主页css样式，构思路由以及视图

--- mallAdmin/urls.py
+++ mallAdmin/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = "mallAdmin"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("index/", views.index, name="index"),
+    # path("", views.index, name="index"),
+    # path("index/", views.index, name="index"),
+    re_path(".*", views.index),
 ]
--- mallAdmin/views.py
+++ mallAdmin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from mall.models import *
 
+
 def index(request):
     request.session["title"] = "后台管理"
     context = {
